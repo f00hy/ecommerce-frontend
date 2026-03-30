5db1aaa865f7329abb53bbd77aed3fcd00800039
f00hy <yaoheng20@gmail.com>
2026-03-31 01:32:53 +0800

    perf: use Link component in CheckoutHeader component
    refs commit 5a06525

--- src/pages/checkout/CheckoutHeader.jsx
+++ src/pages/checkout/CheckoutHeader.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router';
 import './CheckoutHeader.css';
 
 function CheckoutHeader() {
@@ -5,17 +6,17 @@ function CheckoutHeader() {
     <div className="checkout-header">
       <div className="header-content">
         <div className="checkout-header-left-section">
-          <a href="/">
+          <Link to="/">
             <img className="logo" src="images/logo.png" />
             <img className="mobile-logo" src="images/mobile-logo.png" />
-          </a>
+          </Link>
         </div>
 
         <div className="checkout-header-middle-section">
           Checkout (
-          <a className="return-to-home-link" href="/">
+          <Link className="return-to-home-link" to="/">
             3 items
-          </a>
+          </Link>
           )
         </div>
 

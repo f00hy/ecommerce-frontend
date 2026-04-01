059688da37340e6d7659c27f9fe22627ee544ec4
f00hy <yaoheng20@gmail.com>
2026-04-01 21:44:39 +0800

    fix: resolve import problem

--- src/pages/orders/OrdersPage.jsx
+++ src/pages/orders/OrdersPage.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 import Header from '../../components/Header';
+import OrdersGrid from './OrdersGrid';
 import './OrdersPage.css';
 
 function OrdersPage({ cart }) {

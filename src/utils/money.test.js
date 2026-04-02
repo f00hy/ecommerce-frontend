614af5dd1a2007435bc56c05bb2ca9fcc6756bc2
f00hy <yaoheng20@gmail.com>
2026-04-02 17:30:53 +0800

    test: add unit tests for formatMoney function

--- src/utils/money.test.js
+++ src/utils/money.test.js
@@ -14,4 +14,9 @@ describe('formatMoney function', () => {
   it('works with 0 cents', () => {
     expect(formatMoney(0)).toBe('$0.00');
   });
+
+  it('works with negative numbers', () => {
+    expect(formatMoney(-999)).toBe('-$9.99');
+    expect(formatMoney(-100)).toBe('-$1.00');
+  });
 });

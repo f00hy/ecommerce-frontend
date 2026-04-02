181ab8639eb2b172a652422a5fcc6f2eebc33b24
f00hy <yaoheng20@gmail.com>
2026-04-02 20:55:33 +0800

    test: add integration tests for Header component
    displaying header correctly

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -29,14 +29,15 @@ function Header({ cart }) {
     <div className="header">
       <div className="left-section">
         <NavLink to="/" className="header-link">
-          <img className="logo" src={logoWhite} />
-          <img className="mobile-logo" src={mobileLogoWhite} />
+          <img className="logo" data-testid="header-logo" src={logoWhite} />
+          <img className="mobile-logo" data-testid="header-mobile-logo" src={mobileLogoWhite} />
         </NavLink>
       </div>
 
       <div className="middle-section">
         <input
           className="search-bar"
+          data-testid="header-search-bar"
           type="text"
           placeholder="Search"
           value={searchInput}
@@ -44,16 +45,16 @@ function Header({ cart }) {
         />
 
         <button className="search-button" onClick={search}>
-          <img className="search-icon" src={searchIcon} />
+          <img className="search-icon" data-testid="header-search-button" src={searchIcon} />
         </button>
       </div>
 
       <div className="right-section">
-        <NavLink className="orders-link header-link" to="/orders">
+        <NavLink className="orders-link header-link" data-testid="header-orders-link" to="/orders">
           <span className="orders-text">Orders</span>
         </NavLink>
 
-        <NavLink className="cart-link header-link" to="/checkout">
+        <NavLink className="cart-link header-link" data-testid="header-cart-link" to="/checkout">
           <img className="cart-icon" src={cartIcon} />
           <div className="cart-quantity">{totalQuantity}</div>
           <div className="cart-text">Cart</div>
